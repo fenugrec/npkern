e5d68f3c05bb38f7ee514fcf60317eaf357741a0
fenugrec <fenugrec@users.sourceforge.net>
2016-12-20 22:24:00 -0500

    7055_350nm : Initial, untested, write implementation.
    - fixed write pulse delay constants
    - implement write according to datasheet and FDT example code
    - some uncertain points still
    - no informational error values yet
    - untested !

---
+++ extra_functions.h
@@ -0,0 +1,51 @@
+/* Extra asm helpers for SH */
+
+#define FUNCS_DECL static inline
+
+/** Saves unshifted intmask and sets to 0x0F to disable.
+ * Use "imask_restore()" , not set_imask() to re-enable.
+ */
+FUNCS_DECL unsigned imask_savedisable(void) __attribute__ ((always_inline));
+
+/** Restore imask; assumes current imask is 0x0F
+ * Only use with value obtained from imask_savedisable()
+ */
+FUNCS_DECL void imask_restore(unsigned unshifted_mask);
+
+
+extern __inline__ unsigned imask_savedisable() {
+	unsigned val;
+	volatile unsigned tmp;
+
+	asm volatile (
+		"stc   sr,%0\n"
+		"mov   %0, %1\n"
+		"mov   #0xff,r0\n"
+		"add   #0x10, r0\n"
+		"and   r0, %0\n"
+		"not   r0, r0\n"
+		"or    r0, %1\n"
+		"ldc   %1, sr"
+			:"=r"(val),"=r"(tmp)
+			:
+			:"cc","r0"
+	);
+
+	return val;
+}
+
+	
+extern __inline__ void imask_restore(unsigned unshifted_mask) {
+	volatile unsigned tmp2;
+	asm volatile (
+		"stc   sr,r0\n"
+		"mov   #0xff,%0\n"
+		"add   #0x10, %0\n"		//r0 = FFFF FF0F
+		"and   %0,r0\n"
+		"or    %1,r0\n"
+		"ldc   r0,sr"
+			:"=r"(tmp2)
+			:"r" (unshifted_mask)
+			:"r0", "cc"
+	);
+}
--- platf_7055_350nm.c
+++ platf_7055_350nm.c
@@ -19,11 +19,41 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/* General notes
+ *
+ * The 7055 datasheet is written in a slightly confusing style, and some
+ * elements are not perfectly clear.
+ * Renesas FDT includes sample kernel code that *should* work, but
+ * in some respects does not follow the DS, adding to the confusion. The
+ * FDT code is also handwritten assembly, commented in mostly-english, so
+ * the intent is not always clear.
+ *
+ * Other code I've seen (Nissan kernel) is yet another interpretation of
+ * the DS, and disagrees with both FDT code and the DS on some points.
+ *
+ * In here, I've attempted to follow the DS to the letter, referring to
+ * FDT code for correctness and Nissan code for sanity.
+ *
+ **** Questionable points
+ *
+ * Use of WDT peripheral : DS and FDT code use it, Nissan doesn't.
+ * I currently have it in here, but I may remove it since I don't know
+ * if Nissan ECUs have the WDTOVF CPU pin to anything problematic.
+ *
+ * Computation of "additional programming data" : DS is unclear, Nissan
+ * seems wrong, and FDT agrees with my reading of the DS.
+ *
+ * Delay loops : the most critical timing values are the "write pulse"
+ * delays; for these I disable interrupts around the pulse so our ECU_WDT
+ * interrupt doesn't interfere.
+ */
+
 
 #include "functions.h"
+#include "extra_functions.h"
 #include "reg_defines/7055_350nm.h"	//io peripheral regs etc
 
-#include <string.h>	//memcmp
+#include <string.h>	//memcpy
 #include "stypes.h"
 #include "platf.h"
 #include "iso_cmds.h"
@@ -53,7 +83,8 @@
 #define CPUFREQ	(40)
 
 #define WDT_RSTCSR_SETTING 0x5A5F;	//power-on reset if TCNT overflows
-#define WDT_TCSR_START (0xA578 | 0x06)	//write value to start with 1:4096 div (26.2 ms @ 40MHz)
+#define WDT_TCSR_ESTART (0xA578 | 0x06)	//write value to start with 1:4096 div (26.2 ms @ 40MHz), for erase runaway
+#define WDT_TCSR_WSTART (0xA578 | 0x05)	//write value to start with 1:1024 div (6.6 ms @ 40MHz), for write runaway
 #define WDT_TCSR_STOP 0xA558	//write value to stop WDT count
 
 //TODO recheck calculation
@@ -78,8 +109,8 @@
 /** Write timing constants */
 #define TSPSU	WAITN_CALCN(50)
 #define TSP10	WAITN_CALCN(10)
-#define TSP30	WAITN_CALCN(10)
-#define TSP200	WAITN_CALCN(10)
+#define TSP30	WAITN_CALCN(30)
+#define TSP200	WAITN_CALCN(200)
 #define TCP	WAITN_CALCN(5)
 #define TCPSU	WAITN_CALCN(5)
 #define TSPV	WAITN_CALCN(4)
@@ -109,9 +140,10 @@
 
 
 #ifdef ASM_IMPLEM
-/* probably dead-end attempt at porting the Renesas FDT code.
+/* probably dead-end attempt at porting the Renesas FDT code. Little/nothing to be gained
 Size with no implems: 3392	1024	536 => 4952
 Size with Erase asm implem: 4024	1024	552 => 5600, delta = 648B
+Size with Erase C implem: 3924	1024	548	=> 5496
 */
 
 /** Common timing constants */
@@ -211,6 +243,9 @@ static void sweclear(void) {
 	waitn(TCSWE);
 }
 
+
+/*********** Erase ***********/
+
 /** Erase verification
  * Assumes pFLMCR is set, of course
  * ret 1 if ok
@@ -255,7 +290,7 @@ static void ferase(unsigned blockno) {
 	*pEBR = bitsel;
 
 	WDT.WRITE.TCSR = WDT_TCSR_STOP;	//this also clears TCNT
-	WDT.WRITE.TCSR = WDT_TCSR_START;
+	WDT.WRITE.TCSR = WDT_TCSR_ESTART;
 
 	*pFLMCR |= FLMCR_ESU;
 	waitn(TSESU);
@@ -272,29 +307,6 @@ static void ferase(unsigned blockno) {
 
 }
 
-
-
-//TODO : give sane values to these & other errors
-#define FL_ERROR 0x80
-
-bool platf_flash_init(u8 *err) {
-	reflash_enabled = 0;
-
-	//Check FLER
-	if (!fwecheck()) {
-		*err = FL_ERROR;
-		return 0;
-	}
-
-	/* suxxess ! */
-	return 1;
-
-}
-
-
-void platf_flash_unprotect(void) {
-	reflash_enabled = 1;
-}
 
 
 //same error codes as 180nm, for convenience maybe
@@ -327,12 +339,16 @@ uint32_t platf_flash_eb(unsigned blockno) {
 	WDT.WRITE.TCSR = WDT_TCSR_STOP;
 	WDT.WRITE.RSTCSR = WDT_RSTCSR_SETTING;
 
-	//XXX etc
+	/* XXX is this pre-erase verify really necessary ?? DS doesn't say so;
+	 * FDT example has this, and Nissan kernel doesn't
+	 */
+#if 0
 	if (ferasevf(blockno)) {
 		//already blank
 		sweclear();
 		return 0;
 	}
+#endif
 
 
 	for (count = 0; count < MAX_ET; count++) {
@@ -349,13 +365,141 @@ uint32_t platf_flash_eb(unsigned blockno) {
 }
 
 
+
+/*********** Write ***********/
+
+/** write pulse for tps=10/30/200us as specified
+ */
+static void writepulse(unsigned tsp) {
+//	int prev_imask = get_imask();
+//	set_imask(0x0F);
+	unsigned uim = imask_savedisable();
+
+	WDT.WRITE.TCSR = WDT_TCSR_STOP;
+	WDT.WRITE.TCSR = WDT_TCSR_WSTART;
+
+	*pFLMCR |= FLMCR_PSU;
+	waitn(TSPSU);
+	*pFLMCR |= FLMCR_P;
+	waitn(tsp);
+	*pFLMCR &= ~FLMCR_P;
+	waitn(TCP);
+	*pFLMCR &= !FLMCR_PSU;
+	waitn(TCPSU);
+	WDT.WRITE.TCSR = WDT_TCSR_STOP;
+
+//	set_imask(prev_imask);
+	imask_restore(uim);
+}
+
+
 /** ret 0 if ok,
  * assumes params are ok, and that block was already erased
  */
-uint32_t flash_write128(uint32_t dest, uint32_t src) {
-	(void) dest;
-	(void) src;
-	return -1;
+u32 flash_write128(u32 dest, u32 src) {
+	u8 reprog[128] __attribute ((aligned (4)));	// retry / reprogram data
+	u8 addit[128] __attribute ((aligned (4)));	// overwrite / additional data
+	unsigned n;
+	bool m;
+	u32 rv;
+
+	if (dest < fblocks[FLMCR1_MAXBLOCK + 1]) {
+		pFLMCR = &FLASH.FLMCR1.BYTE;
+	} else {
+		pFLMCR = &FLASH.FLMCR2.BYTE;
+	}
+
+	if (!fwecheck()) {
+		return -1;	//XXX
+	}
+
+	memcpy(reprog, (void *) src, 128);
+
+	sweset();
+	WDT.WRITE.TCSR = WDT_TCSR_STOP;
+	WDT.WRITE.RSTCSR = WDT_RSTCSR_SETTING;
+
+	n = 1;
+	while (n < MAX_WT) {
+		unsigned cur;
+
+		m = 0;
+
+		//1) write (latch) to flash, can't use memcpy because this must be u8 transfers
+		for (cur = 0; cur < 128; cur++) {
+			*(volatile u8 *) (dest + cur) = reprog[cur];
+		}
+
+		if (n <= OW_COUNT) {
+			writepulse(TSP30);
+		} else {
+			writepulse(TSP200);
+		}
+
+		//2) Program verify
+		*pFLMCR |= FLMCR_PV;
+		waitn(TSPV);
+
+
+		for (cur = 0; cur < 128; cur += 4) {
+			u32 verifdata;
+			u32 srcdata;
+
+			*(volatile u32 *) (dest + cur) = (u32) -1;	//dummy write 0xFFFFFFFF
+			waitn(TSPVR);
+
+			verifdata = *(volatile u32 *) (dest + cur);
+			srcdata = *(u32 *) (src + cur);
+
+			if (verifdata != *(u32 *) (reprog + cur)) {
+				//mismatch:
+				m = 1;
+			}
+
+			if (n <= 6) {
+				// compute "additional programming data"
+				// The datasheet isn't very clear about this, and interpretations vary (Nissan kernel vs FDT example)
+				// This follows what FDT does.
+				* (u32 *) (addit + cur) = verifdata | (*(u32 *) (reprog + cur));
+
+			}
+			if (src & ~verifdata) {
+				//wanted '1' bits, but somehow got '0's : serious error
+				//XXXX
+				rv = -1;
+				goto badexit;
+			}
+			//compute reprogramming data. This fits with my reading of both the DS and the FDT code,
+			//but Nissan proceeds differently
+            * (u32 *) (reprog + cur) = srcdata | ~verifdata;
+		}
+
+		*pFLMCR &= ~FLMCR_PV;
+		waitn(TCPV);
+
+		if (n <= 6) {
+			// write additional repro data; can't use memcpy because this must be u8 transfers
+			for (cur = 0; cur < 128; cur++) {
+				*(volatile u8 *) (dest + cur) = addit[cur];
+			}
+
+			writepulse(TSP10);
+		}
+
+
+	}	//while n < 1000
+
+	if (!m) {
+		//success
+		sweclear();
+		return 0;
+	}
+
+	//XXX failure (max retries or something)
+	rv = -1;
+badexit:
+	sweclear();
+	return rv;
 }
 
 
@@ -381,11 +525,35 @@ uint32_t platf_flash_wb(uint32_t dest, uint32_t src, uint32_t len) {
 			return rv;	//TODO : tweak into valid NRC
 		}
 
-		if (memcmp((void *)dest, (void *)src, 128) != 0) return PFWB_VERIFAIL;
-
 		dest += 128;
 		src += 128;
 		len -= 128;
 	}
 	return 0;
 }
+
+
+/*********** init, unprotect ***********/
+
+//TODO : give sane values to these & other errors
+#define FL_ERROR 0x80
+
+bool platf_flash_init(u8 *err) {
+	reflash_enabled = 0;
+
+	//Check FLER
+	if (!fwecheck()) {
+		*err = FL_ERROR;
+		return 0;
+	}
+
+	/* suxxess ! */
+	return 1;
+
+}
+
+
+void platf_flash_unprotect(void) {
+	reflash_enabled = 1;
+}
+
